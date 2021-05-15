cb2917177d1b9cb5847bd8ed166d3254b6e0a90b
SalvaChLl <salvachll@gmail.com>
2021-05-15 20:39:51 +0200

    Se ha optimizado la navegacion para mobiles

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 const articles = document.querySelectorAll(".article")
-
+const burger = document.querySelector(".burger")
+const navigation = document.getElementsByTagName('nav')[0]
+console.log(navigation)
 articles.forEach(article => {
   article.addEventListener('click', (e) => {
     article.classList.toggle("active")
   })
 });
+
+burger.addEventListener('click', (e) => {
+  e.preventDefault()
+
+  burger.classList.toggle('active')
+  navigation.classList.toggle('active')
+})
